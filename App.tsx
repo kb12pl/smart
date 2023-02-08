8e59adfa1e6432bfa929d626f55b8ad7e2c23af6
kb12pl <kb12pl@outlook.com>
2023-02-08 16:52:29 +0100

    123

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Cat from './kb/main';
+import {Ok} from './kb/tools';
+
+
+const App = () => {
+  return (
+    
+    <View>
+      <Text>Welcome!</Text>      
+      <Cat />
+    </View>
+  );
+};
+
+export default App;
---
+++ kb/main.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import {Ok} from './tools';
+
+export default function Cat(){
+  return (
+    <View>
+      <Ok/>
+    </View>
+  );
+};
