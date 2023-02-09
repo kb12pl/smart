2c3793ee51f91b996fe19225cf8653a8c5771f10
kb12pl <kb12pl@outlook.com>
2023-02-09 16:19:45 +0100

    123

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import * as React from 'react';
+import { View, Text,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Cat from './kb/main';
-import {Ok} from './kb/tools';
+import { Home } from './kb/home'
+import { Picture  } from './kb/picture'
 
 
-const App = () => {
-  return (
-    
-    <View>
-      <Text>Welcome!</Text>      
-      <Cat />
-    </View>
+
+const Stack = createNativeStackNavigator();
+
+function App() {  
+  return (        
+    <NavigationContainer>            
+      <Stack.Navigator>                
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Picturer" component={Picture} />
+      </Stack.Navigator>      
+    </NavigationContainer>
+
   );
-};
+}
 
 export default App;
---
+++ kb/picture.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text,Button } from 'react-native';
+
+
+export function Picture() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+  
