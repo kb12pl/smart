3aab0b652f321150ac4bb2bca3d8de8353a4246a
kb12pl <kb12pl@outlook.com>
2023-02-14 15:17:06 +0100

    123

--- App.tsx
+++ App.tsx
@@ -3,22 +3,48 @@ import { View, Text,Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home } from './kb/home'
+
 import { Picture  } from './kb/picture'
-
-
+import Layout from './kb/layout'
+import Flex  from './kb/flex'
 
 const Stack = createNativeStackNavigator();
+
+const ButtonGoTo=({label,navigation})=>{
+  return (
+    <Button title={label} onPress={() => {navigation.navigate(label);}}/>      
+  );
+}
+
+const Home=({ navigation })=>{    
+  return (      
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>    
+      <ButtonGoTo label="Picture" navigation={navigation}/>      
+      <ButtonGoTo label="Flex" navigation={navigation}/>      
+      <ButtonGoTo label="Layout" navigation={navigation}/>      
+    </View>      
+  );
+}
+
+const options={    
+  headerRight: () => (
+    <View>
+    <Button onPress={() => alert('This is a button!')} title="Info" color="#fff"  />        
+    </View>
+  ),
+}
+
 
 function App() {  
   return (        
     <NavigationContainer>            
       <Stack.Navigator>                
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Picturer" component={Picture} />
+        <Stack.Screen name="Home" component={Home}  />
+        <Stack.Screen name="Picture" component={Picture} options={options}/>
+        <Stack.Screen name="Flex" component={Flex} options={options}/>
+        <Stack.Screen name="Layout" component={Layout} options={options}/>
       </Stack.Navigator>      
     </NavigationContainer>
-
   );
 }
 
